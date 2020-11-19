647d9cecba534583f9e5fc4f5211faf16c01cf14
thecheatisgod@gmail.com <thecheatisgod@gmail.com>
2020-11-19 08:10:56 -0700

    finalize for demo

--- src/main/java/com/revature/inventory/RevatureInventoryApplication.java
+++ src/main/java/com/revature/inventory/RevatureInventoryApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
 @RibbonClient(name = "rss-inventory-service")
+@EnableDiscoveryClient
 public class RevatureInventoryApplication {
 
 	public static void main(String[] args) {
